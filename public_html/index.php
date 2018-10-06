cde60df0aaeff2d542407e68ee1beb529e4cc1c1
Elvismetsi <elvismetsis@gmail.com>
2018-10-06 16:53:18 +0100

    UI creation

--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+    <title>First Catering LTD</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
 
-<?php
-echo phpinfo(); 
-?>
+<div class="row">
+    <div class="col-md-4">
+        <img class="logo" src="img/logo.png" />
+    </div>      
+    <div class="col-md-4">
+        <h1 class="name"> First Catering LTD </h1>
+    </div>   
+    <div class="col-md-4">
+        <p class="sign-in" ><a href="#"> Sign In </a></p>
+    </div>   
+</div>
+
+<div class="flex-container">
+  <button>Company Registration</button>
+  <button>Employee Registration</button>
+  <button>Company CSV Upload</button>
+</div>
+
 
 </body>
 </html>
